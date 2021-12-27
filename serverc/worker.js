48e0776840ef87f06a82a2143c067cf656ba362d
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-12-27 16:51:52 +0530

    completed the task

--- serverc/worker.js
+++ serverc/worker.js
@@ -1,4 +1,4 @@
-const BillingHandler=require('./helpers/billingQueue');
+const BillingHandler=require('../servera/helpers/billingQueue');
 async function main(){
     console.log('worker is running')
     BillingHandler.processQueue();
